29ffbe352c9b80c2ae42174fc1f289ce87de50da
Christian R. Garcia <christian.g21@gmail.com>
2019-06-13 18:19:01 -0500

    Updated sending.py

--- flops/sending.py
+++ flops/sending.py
@@ -2,20 +2,17 @@ import requests as r
 import multiprocessing
 import time
 
-start = time.time()
 def internet_resource_getter(it):
-    stuff_got = []
-    response = r.post("https://dev.tenants.aloedev.tacc.cloud/actors/v2/qAMA8MLmwGkb3/messages",
+    response = r.post("https://dev.tenants.aloedev.tacc.cloud/actors/v2/YJE3V4qQ5rA5/messages",
                        headers={'Authorization': 'Bearer 74b1aaf8ca3f131071be811964fd2987'},
                        data={'message':'0 1000 2'})
 
-    stuff_got.append(response.json())
+    return response.json()
 
-    return stuff_got
-
-
+start = time.time()
 pool = multiprocessing.Pool(processes=8)
 pool_outputs = pool.map(internet_resource_getter, range(200))
 pool.close()
 pool.join()
 print(time.time() - start)
+print(pool_outputs)
